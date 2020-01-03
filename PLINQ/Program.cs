814fe56817f2f9b8b170d00237c31ad6a0eb4ec2
Georgi Yanakiev <georgiyanakiev@hopsoftware.com>
2020-01-03 17:26:00 +0000

    Add project files.

---
+++ PLINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLINQ
+{
+   public static class Program1
+    {
+         public static void Main()
+        {
+            var num = Enumerable.Range(0, 100);
+            var pResult = num.AsParallel()
+                .Where(i => i % 2 == 0)
+                .ToArray();
+
+            foreach (int i in pResult)
+                Console.WriteLine(i);
+
+        }
+    }
+}
